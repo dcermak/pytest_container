a494802633170c5bef8ba6b8086836516c3d209f
Dan Čermák <dcermak@suse.com>
2021-10-06 13:50:43 +0200

    Raise an error when custom_entry_point and default_entry_point are set

---
+++ tests/base/test_container.py
@@ -0,0 +1,21 @@
+import pytest
+from pytest_container import Container
+from pytest_container import DerivedContainer
+
+
+def test_container_default_entry_point_and_custom_one_set():
+    with pytest.raises(ValueError) as ve:
+        Container(
+            url="ignore", default_entry_point=True, custom_entry_point="foobar"
+        )
+    assert "A custom entry point has been provided" in str(ve.value)
+
+
+def test_derived_container_default_entry_point_and_custom_one_set():
+    with pytest.raises(ValueError) as ve:
+        DerivedContainer(
+            base="foobar",
+            default_entry_point=True,
+            custom_entry_point="foobar",
+        )
+    assert "A custom entry point has been provided" in str(ve.value)
