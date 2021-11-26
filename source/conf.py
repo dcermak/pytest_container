e2f3ac03d3dde357e73ffd4daec66d2d540d1ae1
Dan Čermák <dcermak@suse.com>
2021-11-26 11:47:21 +0100

    Enable intersphinx

--- source/conf.py
+++ source/conf.py
@@ -31,6 +31,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,3 +54,5 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
