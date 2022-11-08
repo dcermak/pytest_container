6ec14d107ae46fcd080167f846efdb1ca8954c72
Dan Čermák <dcermak@suse.com>
2022-11-08 15:36:55 +0100

    Set encoding for open()

--- tests/base/test_copy_from_repo.py
+++ tests/base/test_copy_from_repo.py
@@ -15,7 +15,9 @@ CONTAINER_IMAGES = [LEAP_WITH_CONFIG_FILE]
 
 def test_config_file_present(auto_container, pytestconfig: Config):
     assert auto_container.connection.file("/opt/app/pyproject.toml").exists
-    with open(pytestconfig.rootpath / "pyproject.toml") as pyproject:
+    with open(
+        pytestconfig.rootpath / "pyproject.toml", encoding="utf-8"
+    ) as pyproject:
         assert auto_container.connection.file(
             "/opt/app/pyproject.toml"
         ).content_string == pyproject.read(-1)
