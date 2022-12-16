6a9c08da392f129511708e7478e2e0a9cb0cf62f
Dan Čermák <dcermak@suse.com>
2022-12-16 16:16:38 +0100

    Add hack from https://github.com/agronholm/typeguard/issues/260#issuecomment-1197525481 for typeguard

--- tests/base/conftest.py
+++ tests/base/conftest.py
@@ -3,6 +3,16 @@ from pytest_container import add_extra_run_and_build_args_options
 from pytest_container import add_logging_level_options
 from pytest_container import auto_container_parametrize
 from pytest_container import set_logging_level_from_cli_args
+from typeguard import typechecked
+from typeguard.importhook import install_import_hook
+
+
+def pytest_runtest_call(item):
+    # Decorate every test function [e.g. test_foo()] with typeguard's
+    # typechecked() decorator.
+    test_func = getattr(item, "obj", None)
+    if test_func is not None:
+        setattr(item, "obj", typechecked(test_func))
 
 
 def pytest_generate_tests(metafunc):
@@ -16,3 +26,4 @@ def pytest_addoption(parser):
 
 def pytest_configure(config):
     set_logging_level_from_cli_args(config)
+    install_import_hook("pytest_confainer")
