cd50197860fb3505fb344e0ea05e652e3a3457ca
Jelle van der Waa <jelle@vdwaa.nl>
2023-06-30 19:15:51 +0200

    tests: add typeguard 4 compatibility
    The install_import_hook import changed in version 4.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 # pylint: disable=missing-function-docstring,missing-module-docstring
 from typeguard import typechecked
-from typeguard.importhook import install_import_hook
+try:
+    from typeguard.importhook import install_import_hook
+except ImportError:
+    from typeguard import install_import_hook
 
 from pytest_container import add_extra_run_and_build_args_options
 from pytest_container import add_logging_level_options
